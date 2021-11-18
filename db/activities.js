5dffb8f4989a029b6ae78133117d9d8194bc603c
Mark Clements <mclements@Marks-MBP.attlocal.net>
2021-11-18 14:54:44 -0600

    end of thursday, working on attachActivitiesToRoutines

--- db/activities.js
+++ db/activities.js
@@ -2,7 +2,9 @@ const client = require("./client");
 
 // database functions
 async function getAllActivities() {
-  const { rows } = await client.query(`SELECT * FROM activities;`);
+  const { rows } = await client.query(`
+  SELECT *
+  FROM activities;`);
   return rows;
 }
 
@@ -29,7 +31,23 @@ async function getActivityByName(activityName) {
   return activities;
 }
 
-async function attachActivitiesToRoutines(routines) {}
+async function attachActivitiesToRoutines(routines) {
+  const routinesToReturn = [...routines];
+  const routineInfo = routinesToReturn
+    .map((fieldName, index) => `$${index + 1}`)
+    .join(", ");
+  const routineIds = routinesToReturn.map(
+    (fieldName, index) => `$${index + 1}`
+  );
+  const { rows: activities } = await client.query(
+    `SELECT activities.*, routine_activities.count, routine_activities.duration, routine_activities.id AS "routineActivityId", routine_activities."routineId"
+    FROM activities
+    JOIN routine_activities ON "activityId" = activities.id
+    WHERE routine_activities."routineId" IN (${routineInfo})
+    `,
+    routineIds
+  );
+}
 
 // select and return an array of all activities
 async function createActivity(activity) {
@@ -47,18 +65,28 @@ async function createActivity(activity) {
 }
 
 // return the new activity
-async function updateActivity(activity) {
-  const { name, description } = activity;
-  const {
-    rows: [updateActivity],
-  } = await client.query(
-    `INSERT INTO activities(name, description)
-    VALUES($1,$2)
+async function updateActivity({ id, ...fields }) {
+  const fieldNames = Object.keys(fields);
+
+  const setString = fieldNames
+    .map((fieldName, index) => {
+      return `${fieldName}=$${index + 2}`;
+    })
+    .join(", ");
+
+  const fieldValues = Object.values(fields);
+
+  const { rows } = await client.query(
+    `UPDATE activities SET ${setString}
+    WHERE id = $1
     RETURNING *;
     `,
-    [name, description]
+    [id, ...fieldValues]
   );
-  return updateActivity;
+
+  const [activity] = rows;
+
+  return activity;
 }
 
 // don't try to update the id
--- db/routines.js
+++ db/routines.js
@@ -1,34 +1,59 @@
-const client = require('./client');
+const { getUserById } = require(".");
+const client = require("./client");
+const { attachActivitiesToRoutines } = require("./activities");
 
-async function getRoutineById(id){
+async function getRoutineById(id) {
+  const {
+    rows: [routine],
+  } = await client.query(
+    `SELECT *
+    FROM routines
+    WHERE id=$1`,
+    [id]
+  );
+  return routine;
 }
 
-async function getRoutinesWithoutActivities(){
+async function getRoutinesWithoutActivities() {
+  const { rows } = await client.query(`
+  SELECT id, "routineId", duration, count
+  FROM routines;`);
+  return rows;
 }
 
 async function getAllRoutines() {
+  const { rows } = await client.query(`
+  SELECT routines.*, users.username AS "creatorName"
+  FROM routines
+  JOIN users ON "creatorId" = users.id`);
+  console.log("ROWSSSSSSS:", rows);
+  // return rows;
+  return attachActivitiesToRoutines(rows);
 }
 
-async function getAllRoutinesByUser({username}) {
+async function getAllRoutinesByUser({ username }) {}
+
+async function getPublicRoutinesByUser({ username }) {}
+
+async function getAllPublicRoutines() {}
+
+async function getPublicRoutinesByActivity({ id }) {}
+
+async function createRoutine({ creatorId, isPublic, name, goal }) {
+  const {
+    rows: [newRoutine],
+  } = await client.query(
+    `INSERT INTO routines("creatorId", "isPublic", name, goal)
+    VALUES($1, $2, $3, $4)
+    RETURNING *;`,
+    [creatorId, isPublic, name, goal]
+  );
+  return newRoutine;
 }
 
-async function getPublicRoutinesByUser({username}) {
-}
+async function updateRoutine({ id, ...fields }) {}
 
-async function getAllPublicRoutines() {
-}
-
-async function getPublicRoutinesByActivity({id}) {
-}
-
-async function createRoutine({creatorId, isPublic, name, goal}) {
-}
-
-async function updateRoutine({id, ...fields}) {
-}
-
-async function destroyRoutine(id) {
-}
+async function destroyRoutine(id) {}
 
 module.exports = {
   getRoutineById,
@@ -41,4 +66,4 @@ module.exports = {
   createRoutine,
   updateRoutine,
   destroyRoutine,
-}
+};
